58eb10002ea8fe78f110348f498f35ef517e54b4
Rafael Aquino <rafael.aquinoquijano@gmail.com>
2026-01-13 18:34:45 -0600

    python_utils added

---
+++ python_utils/test.py
@@ -0,0 +1,13 @@
+# import kagglehub
+# https://www.kaggle.com/datasets/nezahatkk/10-000-english-words-cerf-labelled
+# Download latest version
+# path = kagglehub.dataset_download("nezahatkk/10-000-english-words-cerf-labelled")
+
+
+
+
+def main():
+    print("Hello World")
+    # print("Path to dataset files:", path)
+if __name__ == '__main__':
+    main()
